8da9244f970caaf68cc99ab9547702297550d574
SAMYAR_SR <s.feroni@domil.io>
2024-10-09 11:04:16 +0330

    new Bani

---
+++ src/app/Component/HeadTop.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+
+function HeadTop() {
+  return (
+    <div>
+
+
+        <div className='flex h-screen justify-center items-center'>
+            <Image src="/Images/banimodeBigLogo.png" alt='a'height={1000} width={1000}  />
+            <div className="self-stretch flex flex-col text-white text-center font-bold text-[28px] leading-[36px]"><h1>نرفــــــــــته برمــــــــــی‌گرده!</h1></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+
+
+
+        <div></div>
+      
+    </div>
+  )
+}
+
+export default HeadTop
